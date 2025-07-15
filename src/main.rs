c0d9a7f6a191563d33a11070f3f76ec449dd11d7
JuleeC <juleshummel44@gmail.com>
2025-07-15 14:17:29 +0000

    handling all imports

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
-use crossterm::*;
-use ratatui::*;
+use crossterm::{
+    event::{self, Event, KeyCode},
+    execute,
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
+};
+use ratatui::{
+    Terminal,
+    backend::CrosstermBackend,
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Style},
+    widgets::{Block, Borders, List, ListItem},
+};
 use std::io;
 
 fn main() -> Result<(), io::Error> {
     println!("testing");
+    //declaring
+    let mut stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    execute!(stdout, EnterAlternateScreen)?;
+
+    Ok(())
 }
