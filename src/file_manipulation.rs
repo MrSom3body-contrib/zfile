6f8346daee69788e43a9c983c7dededce1b25dea
JuleeC <juleshummel44@gmail.com>
2025-07-31 13:39:52 +0000

    codingg..:

--- src/file_manipulation.rs
+++ src/file_manipulation.rs
@@ -1,4 +1,9 @@
-fn rename_file() {}
+use std::fs;
+use std::io;
+use std::path::PathBuf;
+fn rename_file(from: &PathBuf, to: &PathBuf) -> io::Result<()> {
+    fs::rename(from, to)
+}
 fn delete_file() {}
 fn copy_file() {}
 fn move_file() {}
