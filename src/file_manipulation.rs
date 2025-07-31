fe16cfecd9efc1c7ee5d461f502e27a98127bb75
JuleeC <juleshummel44@gmail.com>
2025-07-31 13:40:56 +0000

    .

--- src/file_manipulation.rs
+++ src/file_manipulation.rs
@@ -4,6 +4,12 @@ use std::path::PathBuf;
 fn rename_file(from: &PathBuf, to: &PathBuf) -> io::Result<()> {
     fs::rename(from, to)
 }
-fn delete_file() {}
+fn delete_file(path: &PathBuf) -> io::Result<()> {
+    if path.is_dir() {
+        fs::remove_dir_all(path)
+    } else {
+        fs::remove_file(path)
+    }
+}
 fn copy_file() {}
 fn move_file() {}
