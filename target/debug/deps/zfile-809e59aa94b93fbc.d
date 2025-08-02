3062596d6b6a95e0c4fdf7682753b7f5a13574f0
JuleeC <juleshummel44@gmail.com>
2025-08-02 14:38:33 +0000

    delete is working

--- target/debug/deps/zfile-809e59aa94b93fbc.d
+++ target/debug/deps/zfile-809e59aa94b93fbc.d
@@ -1,5 +1,6 @@
-/home/julee/dev/zfile/target/debug/deps/zfile-809e59aa94b93fbc.d: src/main.rs
+/home/julee/dev/zfile/target/debug/deps/zfile-809e59aa94b93fbc.d: src/main.rs src/file_manipulation.rs
 
-/home/julee/dev/zfile/target/debug/deps/zfile-809e59aa94b93fbc: src/main.rs
+/home/julee/dev/zfile/target/debug/deps/zfile-809e59aa94b93fbc: src/main.rs src/file_manipulation.rs
 
 src/main.rs:
+src/file_manipulation.rs:
