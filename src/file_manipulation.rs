16f68e1d10947f9a932c7e5104ec47a23b8888f1
JuleeC <juleshummel44@gmail.com>
2025-08-06 17:52:44 +0000

    readding file mani.. file

---
+++ src/file_manipulation.rs
@@ -0,0 +1,35 @@
+use std::fs;
+use std::io;
+use std::path::PathBuf;
+
+pub fn rename_file(old_path: &PathBuf, new_name: &str) -> io::Result<()> {
+    let parent = old_path.parent().ok_or_else(|| {
+        io::Error::new(io::ErrorKind::Other, "Could not determine parent directory")
+    })?;
+    let new_path = parent.join(new_name);
+    fs::rename(old_path, new_path)?;
+    Ok(())
+}
+
+pub fn move_file(old_path: &PathBuf, new_path_str: &str) -> io::Result<()> {
+    let new_path = PathBuf::from(new_path_str);
+    if new_path.is_dir() {
+        let file_name = old_path
+            .file_name()
+            .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "Invalid file name"))?;
+        let destination = new_path.join(file_name);
+        fs::rename(old_path, destination)?;
+    } else {
+        fs::rename(old_path, new_path)?;
+    }
+    Ok(())
+}
+
+pub fn delete_file(path: &PathBuf) -> io::Result<()> {
+    if path.is_dir() {
+        fs::remove_dir_all(path)?;
+    } else {
+        fs::remove_file(path)?;
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod file_manipulation;
 use crossterm::{
     event::{self, Event, KeyCode},
     execute,
